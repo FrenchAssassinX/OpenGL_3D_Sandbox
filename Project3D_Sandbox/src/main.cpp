490dd6510c6160bc4c1cabd8997bf83825ddaaa3
Aurélien Thazet <thazet.aurelien@gmail.com>
2020-04-07 14:50:38 +0200

    Problem on runtime

--- Project3D_Sandbox/src/main.cpp
+++ Project3D_Sandbox/src/main.cpp
@@ -1,6 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "Shader.h"
+
 #include <stdio.h>
+#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -10,16 +17,25 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"layout (location = 1) in vec3 aTexCoord;\n"
+	"out vec3 ourColor;\n"
+	"out vec2 texCoord;\n"
 	"void main()\n"
 	"{\n"
-	" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	" gl_Position = vec4(aPos, 1.0);\n"
+	" ourColor = aColor;\n"
+	" textCoord = aTexCoord;\n"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
+	"in vec2 texCoord;\n"
+	"uniform sampler2D ourTexture;\n"
 	"void main()\n"
 	"{\n"
-	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"   FragColor = texture(ourTexture, texCoord);\n"
 	"}\n\0";
 
 int main()
@@ -103,21 +119,15 @@ int main()
 	/* ==================================================================================================================== */
 
 
-	/*float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
-	};*/
+	Shader ourShader("4.1.texture.vs", "4.1.texture.fs");
 
+	/* Rectangle */
 	float vertices[] = {
-	 0.5f,  0.5f, 0.0f,  // top right
-	 0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
-	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,			// top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,			// bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,			// bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f			// top left 
 	};
 
 	unsigned int VAO, VBO, EBO;
@@ -130,11 +140,47 @@ int main()
 	/* Copy vertices array in a buffer for OpenGL to use */
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);											// Buffering rectangle
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	/* Set the vertex attributes pointers */
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Position attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// Color attributes
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// Texture attributes
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	/* ======================================== Textures ================================================================ */
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	/* Set the texture wrapping/filtering options (on the currently bound texture object) */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	/* Load the generate texture */
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("./Assets/img/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Failed to load texture");
+	}
+	stbi_image_free(data);
+
+	/* ====================================== End Textures ============================================================== */
 
 
 	/* ======================================== Render Loop ============================================================== */
@@ -147,10 +193,20 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Draw the object */
-		glUseProgram(shaderProgram); // Use shader program when we want to render an object
+		//glUseProgram(shaderProgram); // Use shader program when we want to render an object 
+
+		//float timeValue = glfwGetTime();
+		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		ourShader.use();
 		glBindVertexArray(VAO);
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	// Drawing polygons in wireframe mode
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);				// Drawing polygons in wireframe mode
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);		// Drawing rectangle
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glBindVertexArray(VAO);
 
 		/* Check and call events and swap the buffers */
